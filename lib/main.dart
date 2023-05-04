5b3096bcde46abb84b67febf817f2c7db8ffe8e4
abdulrouf <roufraghozar1400@gmail.com>
2023-05-04 01:28:47 -0700

    add all to magic

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,47 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     const MaterialApp(
-      home: null,
+      home: BallPage(),
     ),
   );
+
+}
+
+class BallPage extends StatelessWidget {
+  const BallPage({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(title: Text('ask me anyting'),),
+      body: Ball(),
+
+
+    );
+  }
+}
+class Ball extends StatefulWidget {
+  //const Ball({Key? key}) : super(key: key);
+
+  @override
+  State<Ball> createState() => _BallState();
+
+}
+class _BallState extends State<Ball> {
+  //const _BallState({Key? key}) : super(key: key);
+  int ballNumber =0;
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: TextButton(child: Image.asset('images/Ball$ballNumber.png'),onPressed: (){
+      setState(() {
+        ballNumber = Random().nextint(5) +1 ;
+      });
+
+      print('i got clicked: $ballNumber');
+    }),);
+  }
+
+  Random() {}
 }
